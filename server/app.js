2b6ac2176eaaf6b29019cf5fe53a819e0cefa214
Jake <jlw6563@rit.edu>
2025-11-09 22:56:19 -0500

    Domo Maker B

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const favicon = require('serve-favicon');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
+const session = require('express-session');
 
 const router = require('./router.js');
 
@@ -12,10 +13,10 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 mongoose.connect(dbURI).catch((err) => {
-  if (err) {
-    console.log('Could not connect to the database');
-    throw err;
-  }
+    if (err) {
+        console.log('Could not connect to the database');
+        throw err;
+    }
 });
 
 const app = express();
@@ -27,6 +28,13 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(session({
+    key: 'sessionid',
+    secret: 'Domo Arigato',
+    resave: false,
+    saveUninitialized: false,
+}));
+
 app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
@@ -34,6 +42,6 @@ app.set('views', `${__dirname}/../views`);
 router(app);
 
 app.listen(port, (err) => {
-  if (err) { throw err; }
-  console.log(`Listening on port ${port}`);
+    if (err) { throw err; }
+    console.log(`Listening on port ${port}`);
 });
